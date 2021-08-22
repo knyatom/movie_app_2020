c8b8fa739f744a61e54a2caf013a99047f8c0926
kny <knyatom@daum.net>
2021-08-22 23:46:23 +0900

    0822

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import './App.css';
-//import Potato from './Potato';
-function Potato({fav}){
-  return (
-        <div>
-          <h1> I love Potato !!!{fav} 추가요?</h1>
-          <h2> {fav} 또 추가요</h2>
-        </div>
-       )
-}
-function Food({fav2, pic}){
+import Protypes from 'prop-types';
+
+
+function Food({fav2, pic, rating}){
   return (
     <div>
     <h1>I Like {fav2}</h1>
     <img src={pic} style={{width:'100px', border:'3px solid red'}} alt={fav2} />
+    <h4>{rating}/5.0</h4>
     </div>
   )
 }
 const foodLike=[
   { id:1,name:'kimchi',
-  image:'https://img.huffingtonpost.com/asset/5ef1bbda2500002930eb3d3e.jpeg?cache=A4S3uCXvP2&ops=crop_0_254_2744_1936,scalefit_630_noupscale'},
-  { id:2,name:'ramun', image:'https://mblogthumb-phinf.pstatic.net/MjAyMDA1MjZfMjcw/MDAxNTkwNDgxNTI0OTI5.cElkTRG2bnYaUn0RlCq58_227T8KT_g2H1riUFYdYmYg.ZxnDFMZfaOq-jt4Qa5lk2gz3kr1Z-OG7HTVRiLYGEsAg.JPEG.naverschool/GettyImages-647266676.jpg?type=w800'},
+  image:'https://img.huffingtonpost.com/asset/5ef1bbda2500002930eb3d3e.jpeg?cache=A4S3uCXvP2&ops=crop_0_254_2744_1936,scalefit_630_noupscale',
+rating:5},
+  { id:2,name:'ramun', image:'https://mblogthumb-phinf.pstatic.net/MjAyMDA1MjZfMjcw/MDAxNTkwNDgxNTI0OTI5.cElkTRG2bnYaUn0RlCq58_227T8KT_g2H1riUFYdYmYg.ZxnDFMZfaOq-jt4Qa5lk2gz3kr1Z-OG7HTVRiLYGEsAg.JPEG.naverschool/GettyImages-647266676.jpg?type=w800',
+rating:4},
   { id:3,name:'guksoo', image:'https://img.huffingtonpost.com/asset/5ef1bbda2500002930eb3d3e.jpeg?cache=A4S3uCXvP2&ops=crop_0_254_2744_1936,scalefit_630_noupscale'},
   { id:4,name:'된장찌개', image:'https://img.huffingtonpost.com/asset/5ef1bbda2500002930eb3d3e.jpeg?cache=A4S3uCXvP2&ops=crop_0_254_2744_1936,scalefit_630_noupscale'},
 ]
@@ -27,23 +24,27 @@ const foodLike=[
 function App() {
 
  const renderFood = (dish) => {
-    return <Food key={dish.id} fav2={dish.name} pic={dish.image} />
+    return <Food key={dish.id} fav2={dish.name} pic={dish.image} rating={dish.rating} />
   }
 console.log(foodLike.map(renderFood));
   return (
     <div className="App">
       <h1> Hello</h1>
-      <Potato fav="양념감자" something={true} papa={['hellow',1,2,3,4,true]} />
-      <Potato fav="마늘감자"  />
-      <Potato fav="치킨감자"  />
+     
       <hr />
-      <Food fav2={foodLike[0].name}/>
-      <Food fav2={foodLike[1].name}/>
-      <Food fav2={foodLike[2].name}/>
-      <Food fav2={foodLike[3].name}/><hr />
+     
       {foodLike.map(renderFood)}
     </div>
   );
 }
+
+
+Food.prototype={
+  name:Protypes.string.isRequired,
+  picture:Protypes.string.isRequired,
+  rating:Protypes.string.isRequired,
+  
+}
+
 
 export default App;
