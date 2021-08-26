b1cb7f13afdf80e8a001f46e39caea812be955b2
kny <knyatom@daum.net>
2021-08-27 01:00:06 +0900

    8727

--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import  Prototypes  from 'prop-types'
+import './Movie.css';
 
-function Movie({id,title,year,sumary,poster}) {
+function Movie({title,year,sumary,poster}) {
     return (
-        <h4>{title}</h4>
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie__data">
+                <h3 className="movie__title" >{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <p className="movie_summary">{sumary}</p>
+            </div>
+        </div>
     )
 }
 Movie.prototypes = { 
-    id: Prototypes.number.isRequired,
+    // id: Prototypes.number.isRequired,
     year:Prototypes.number.isRequired,
     title:Prototypes.string.isRequired,
     summary:Prototypes.string.isRequired,
